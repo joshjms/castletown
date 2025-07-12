363396b2cbe29a6e43d2769462fbbdf9e3c7b98a
joshjms <joshjms1607@gmail.com>
2025-07-12 22:56:03 +0800

    tests: Simplify tests
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- sandbox/sandbox_test.go
+++ sandbox/sandbox_test.go
@@ -51,177 +51,29 @@ func TestSandboxRun(t *testing.T) {
 func TestSandboxCompileExec(t *testing.T) {
 	Init()
 
-	rootfsDir := "/tmp/_tmp_gcc_15-bookworm"
+	expectedStatus := STATUS_OK
+	expectedOutput := "don't forget\n"
 
-	compileConfig := &Config{
-		RootfsImageDir: rootfsDir,
-		Args:           []string{"g++", "main.cpp", "-o", "main"},
-		Cwd:            "/",
-		Env: []string{
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		},
-		UserNamespace: &UserNamespaceConfig{
-			RootUID:     uint32(os.Getuid()),
-			UIDMapStart: 100000,
-			UIDMapCount: 65534,
-			RootGID:     uint32(os.Getgid()),
-			GIDMapStart: 100000,
-			GIDMapCount: 65534,
-		},
-		TimeLimitMs: 5000,
-		Cgroup: &CgroupConfig{
-			CpuQuota: 100000,
-			Memory:   512 * 1024 * 1024,
-		},
-		Copy: []File{
-			{
-				Src: "test_files/hello.cpp",
-				Dst: "/main.cpp",
-			},
-		},
-		Save: []File{
-			{
-				Src: "/main",
-				Dst: "./main",
-			},
-		},
+	tc := Testcase{
+		File:           "test_files/hello.cpp",
+		ExpectedStatus: &expectedStatus,
+		ExpectedOutput: &expectedOutput,
+		TimeLimitMs:    1000,
 	}
 
-	compileSandbox := NewSandbox(uuid.New().String(), compileConfig)
-	ctx := context.Background()
-
-	compileReport, err := compileSandbox.Run(ctx)
-	require.NoError(t, err, "failed to execute sandbox: %v", err)
-	defer os.Remove("main")
-	require.Equal(t, compileReport.ExitCode, 0, "exit code != 0")
-
-	t.Logf("Compile executed successfully: %+v", compileReport)
-
-	execConfig := &Config{
-		RootfsImageDir: rootfsDir,
-		Args:           []string{"./main"},
-		Cwd:            "/",
-		Env: []string{
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		},
-		UserNamespace: &UserNamespaceConfig{
-			RootUID:     uint32(os.Getuid()),
-			UIDMapStart: 100000,
-			UIDMapCount: 65534,
-			RootGID:     uint32(os.Getgid()),
-			GIDMapStart: 100000,
-			GIDMapCount: 65534,
-		},
-		TimeLimitMs: 1000,
-		Cgroup: &CgroupConfig{
-			CpuQuota:  100000,
-			Memory:    512 * 1024 * 1024,
-			PidsLimit: 1,
-		},
-		Copy: []File{
-			{
-				Src: "main",
-				Dst: "main",
-			},
-		},
-	}
-
-	execSandbox := NewSandbox(uuid.New().String(), execConfig)
-	ctx = context.Background()
-
-	execReport, err := execSandbox.Run(ctx)
-	require.NoError(t, err, "failed to execute sandbox: %v", err)
-
-	require.Equal(t, execReport.ExitCode, 0, "exit code != 0")
-	require.Equal(t, execReport.Stdout, "don't forget\n", "stdout is different")
-
-	t.Logf("Exec executed successfully: %+v", execReport)
+	require.NoError(t, tc.Run())
 }
 
 func TestSandboxTimeLimitExceeded(t *testing.T) {
 	Init()
 
-	rootfsDir := "/tmp/_tmp_gcc_15-bookworm"
+	expectedStatus := STATUS_TIME_LIMIT_EXCEEDED
 
-	compileConfig := &Config{
-		RootfsImageDir: rootfsDir,
-		Args:           []string{"g++", "main.cpp", "-o", "main"},
-		Cwd:            "/",
-		Env: []string{
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		},
-		UserNamespace: &UserNamespaceConfig{
-			RootUID:     uint32(os.Getuid()),
-			UIDMapStart: 100000,
-			UIDMapCount: 65534,
-			RootGID:     uint32(os.Getgid()),
-			GIDMapStart: 100000,
-			GIDMapCount: 65534,
-		},
-		TimeLimitMs: 5000,
-		Cgroup: &CgroupConfig{
-			CpuQuota: 100000,
-			Memory:   512 * 1024 * 1024,
-		},
-		Copy: []File{
-			{
-				Src: "test_files/tle.cpp",
-				Dst: "/main.cpp",
-			},
-		},
-		Save: []File{
-			{
-				Src: "/main",
-				Dst: "./main",
-			},
-		},
+	tc := Testcase{
+		File:           "test_files/tle.cpp",
+		ExpectedStatus: &expectedStatus,
+		TimeLimitMs:    1000,
 	}
 
-	compileSandbox := NewSandbox(uuid.New().String(), compileConfig)
-	ctx := context.Background()
-
-	compileReport, err := compileSandbox.Run(ctx)
-	defer os.Remove("main")
-	require.NoError(t, err, "failed to execute sandbox")
-	require.Equal(t, compileReport.ExitCode, 0, "exit code != 0")
-
-	t.Logf("Compile executed successfully: %+v", compileReport)
-
-	execConfig := &Config{
-		RootfsImageDir: rootfsDir,
-		Args:           []string{"./main"},
-		Cwd:            "/",
-		Env: []string{
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		},
-		UserNamespace: &UserNamespaceConfig{
-			RootUID:     uint32(os.Getuid()),
-			UIDMapStart: 100000,
-			UIDMapCount: 65534,
-			RootGID:     uint32(os.Getgid()),
-			GIDMapStart: 100000,
-			GIDMapCount: 65534,
-		},
-		TimeLimitMs: 1000,
-		Cgroup: &CgroupConfig{
-			CpuQuota:  100000,
-			Memory:    128 * 1024 * 1024,
-			PidsLimit: 1,
-		},
-		Copy: []File{
-			{
-				Src: "main",
-				Dst: "main",
-			},
-		},
-	}
-
-	execSandbox := NewSandbox(uuid.New().String(), execConfig)
-	ctx = context.Background()
-
-	execReport, err := execSandbox.Run(ctx)
-	require.NoError(t, err, "failed to execute sandbox")
-	require.Equal(t, execReport.Status, STATUS_TIME_LIMIT_EXCEEDED, "")
-
-	t.Logf("Exec executed successfully: %+v", execReport)
+	require.NoError(t, tc.Run())
 }
---
+++ sandbox/test_utils.go
@@ -0,0 +1,116 @@
+package sandbox
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/uuid"
+)
+
+type Testcase struct {
+	File  string
+	Stdin string
+
+	ExpectedStatus *Status
+	ExpectedOutput *string
+
+	TimeLimitMs int64
+}
+
+func (tc *Testcase) Run() error {
+	rootfsDir := "/tmp/_tmp_gcc_15-bookworm"
+
+	compileConfig := &Config{
+		RootfsImageDir: rootfsDir,
+		Args:           []string{"g++", "main.cpp", "-o", "main"},
+		Cwd:            "/",
+		Env: []string{
+			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+		},
+		UserNamespace: &UserNamespaceConfig{
+			RootUID:     uint32(os.Getuid()),
+			UIDMapStart: 100000,
+			UIDMapCount: 65534,
+			RootGID:     uint32(os.Getgid()),
+			GIDMapStart: 100000,
+			GIDMapCount: 65534,
+		},
+		TimeLimitMs: tc.TimeLimitMs,
+		Cgroup: &CgroupConfig{
+			CpuQuota: 100000,
+			Memory:   512 * 1024 * 1024,
+		},
+		Copy: []File{
+			{
+				Src: tc.File,
+				Dst: "/main.cpp",
+			},
+		},
+		Save: []File{
+			{
+				Src: "/main",
+				Dst: "./main",
+			},
+		},
+	}
+
+	compileSandbox := NewSandbox(uuid.New().String(), compileConfig)
+	ctx := context.Background()
+
+	compileReport, err := compileSandbox.Run(ctx)
+	defer os.Remove("main")
+	if err != nil {
+		return err
+	}
+	if compileReport.Status != STATUS_OK {
+		return fmt.Errorf("status not ok")
+	}
+
+	execConfig := &Config{
+		RootfsImageDir: rootfsDir,
+		Args:           []string{"./main"},
+		Cwd:            "/",
+		Env: []string{
+			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+		},
+		UserNamespace: &UserNamespaceConfig{
+			RootUID:     uint32(os.Getuid()),
+			UIDMapStart: 100000,
+			UIDMapCount: 65534,
+			RootGID:     uint32(os.Getgid()),
+			GIDMapStart: 100000,
+			GIDMapCount: 65534,
+		},
+		TimeLimitMs: 1000,
+		Cgroup: &CgroupConfig{
+			CpuQuota:  100000,
+			Memory:    128 * 1024 * 1024,
+			PidsLimit: 1,
+		},
+		Copy: []File{
+			{
+				Src: "main",
+				Dst: "main",
+			},
+		},
+	}
+
+	execSandbox := NewSandbox(uuid.New().String(), execConfig)
+	ctx = context.Background()
+
+	execReport, err := execSandbox.Run(ctx)
+	if err != nil {
+		return err
+	}
+
+	if tc.ExpectedStatus != nil && execReport.Status != *tc.ExpectedStatus {
+		return fmt.Errorf("status != expectedStatus, expected: %v, got: %v", *tc.ExpectedStatus, execReport.Status)
+	}
+
+	if tc.ExpectedOutput != nil && execReport.Stdout != *tc.ExpectedOutput {
+		return fmt.Errorf("output != expectedOutput, expected: %v, got: %v", *tc.ExpectedOutput, execReport.Stdout)
+	}
+
+	return nil
+}
