3a2e404157dbd2d1b2b568b3daf5acfdade392b2
joshjms <joshjms1607@gmail.com>
2025-10-09 02:22:12 +0800

    server: fix exec handler to support job appends
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- server/handler/exec/api.go
+++ server/handler/exec/api.go
@@ -1,27 +1,17 @@
 package exec
 
-import "github.com/joshjms/castletown/sandbox"
+import (
+	"github.com/joshjms/castletown/job"
+	"github.com/joshjms/castletown/sandbox"
+)
 
 type Request struct {
-	ID    string    `json:"id"`
-	Files []File    `json:"files"`
-	Procs []Process `json:"steps"`
+	ID    string        `json:"id"`
+	Files []job.File    `json:"files"`
+	Procs []job.Process `json:"steps"`
 }
 
-type File struct {
-	Name    string `json:"name"`
-	Content string `json:"content"`
+type Response struct {
+	ID      string           `json:"id"`
+	Reports []sandbox.Report `json:"reports"`
 }
-
-type Process struct {
-	Image         string   `json:"image"`
-	Cmd           []string `json:"cmd"`
-	Stdin         string   `json:"stdin"`
-	MemoryLimitMB int64    `json:"memoryLimitMB"`
-	TimeLimitMs   uint64   `json:"timeLimitMs"`
-	ProcLimit     int64    `json:"procLimit"`
-	Files         []string `json:"files"`
-	Persist       []string `json:"persist"`
-}
-
-type Response []sandbox.Report
--- server/handler/exec/exec.go
+++ server/handler/exec/exec.go
@@ -7,18 +7,11 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/joshjms/castletown/job"
 	"github.com/joshjms/castletown/sandbox"
 )
 
-type ExecHandler struct {
-	m *sandbox.Manager
-}
-
-func NewExecHandler(m *sandbox.Manager) *ExecHandler {
-	return &ExecHandler{m: m}
-}
-
-func (h *ExecHandler) Handler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -31,102 +24,45 @@ func (h *ExecHandler) Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	req.ID = uuid.NewString()
+	if req.ID == "" {
+		req.ID = uuid.NewString()
+	}
 
-	reports, err := h.handleRequest(r.Context(), req)
+	reports, err := handleRequest(r.Context(), req)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("error running processes: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	reportsJson, err := json.MarshalIndent(reports, "", "  ")
+	response := Response{
+		ID:      req.ID,
+		Reports: reports,
+	}
+
+	responseJson, err := json.MarshalIndent(response, "", "  ")
 	if err != nil {
 		http.Error(w, fmt.Sprintf("cannot marshal reports: %v", err), http.StatusInternalServerError)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(reportsJson)
+	w.Write(responseJson)
 }
 
-func (h *ExecHandler) handleRequest(ctx context.Context, req Request) (Response, error) {
-	if len(req.Procs) == 0 {
-		return nil, fmt.Errorf("no processes specified")
+func handleRequest(ctx context.Context, req Request) ([]sandbox.Report, error) {
+	j := job.Job{
+		ID:    req.ID,
+		Files: req.Files,
+		Procs: req.Procs,
 	}
 
-	if err := verifyImages(req.Procs); err != nil {
-		return nil, fmt.Errorf("invalid images: %w", err)
-	}
+	jp := job.GetJobPool()
+	_job := jp.AddOrAppendJob(&j)
 
-	prepareFileDirs(req.ID, req.Procs)
-	fileDeps, err := getFileDependencies(req.ID, req.Procs, req.Files)
+	reports, err := _job.ExecuteAll(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("error getting file dependencies: %w", err)
-	}
-
-	var reports Response
-
-	for i, proc := range req.Procs {
-		cfg := getDefaultConfig()
-		cfg.Args = proc.Cmd
-		cfg.RootfsImageDir = getImageDir(proc.Image)
-		cfg.BoxDir = getProcFileDir(req.ID, i)
-		cfg.Files = fileDeps[i]
-
-		if proc.TimeLimitMs > 0 {
-			cfg.TimeLimitMs = int64(proc.TimeLimitMs)
-		}
-		if proc.MemoryLimitMB > 0 {
-			cfg.Cgroup.Memory = int64(proc.MemoryLimitMB) * 1024 * 1024
-		}
-		cfg.Stdin = proc.Stdin
-
-		containerId := fmt.Sprintf("%s-%d", req.ID, i)
-		s, err := h.m.NewSandbox(containerId, cfg)
-		if err != nil {
-			return nil, fmt.Errorf("cannot create sandbox for process %d: %v", i, err)
-		}
-		defer h.m.DestroySandbox(containerId)
-
-		report, err := s.Run(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("error running process %d: %v", i, err)
-		}
-
-		reports = append(reports, *report)
+		return nil, fmt.Errorf("error executing job: %w", err)
 	}
 
 	return reports, nil
-}
-
-func getDefaultConfig() *sandbox.Config {
-	return &sandbox.Config{
-		Env: []string{
-			"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		},
-		Cwd:         "/box",
-		TimeLimitMs: 1000,
-		Cgroup: &sandbox.CgroupConfig{
-			CpuShares:  100000,
-			CpuQuota:   100000,
-			Memory:     256 * 1024 * 1024,
-			PidsLimit:  100,
-			CpusetCpus: "0",
-			CpusetMems: "0",
-		},
-		Rlimit: &sandbox.RlimitConfig{
-			Core: &sandbox.Rlimit{
-				Hard: 0,
-				Soft: 0,
-			},
-			Fsize: &sandbox.Rlimit{
-				Hard: 1 * 1024 * 1024,
-				Soft: 1 * 1024 * 1024,
-			},
-			NoFile: &sandbox.Rlimit{
-				Hard: 64,
-				Soft: 64,
-			},
-		},
-	}
 }
